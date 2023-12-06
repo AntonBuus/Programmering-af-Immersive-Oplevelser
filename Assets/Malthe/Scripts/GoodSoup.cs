82d57ed913ae7574b9e501ffbfae3ec501172834
Malthe333 <malthekh@hotmail.com>
2023-12-06 19:14:09 +0100

    Nye skuffer

---
+++ Assets/Malthe/Scripts/GoodSoup.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodSoup : MonoBehaviour
+{
+
+    public GameObject prefab1;
+    public GameObject prefab2;
+
+    public bool HaveSpawned = false;
+    public bool IsTomatoin = false;
+    public bool isCarrotin = false;
+    public bool isSteakin = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TomatoTrue()
+    {
+        IsTomatoin=true;
+    }
+
+    public void CarrotTrue()
+    {
+        isCarrotin=true;
+    }
+
+    public void SteakTrue()
+    {
+        isSteakin=true;
+    }
+
+    public void SwitchtoSoup()
+    {
+        if (HaveSpawned == false && IsTomatoin == true && isCarrotin == true && isSteakin == true)
+        {
+            Invoke("SwitchObjects", 3f);
+        }
+
+
+
+    }    // Update is called once per frame
+    void SwitchObjects()
+    {
+        // Destroy the first prefab
+        Destroy(prefab1);
+
+        // Instantiate the second prefab
+        Instantiate(prefab2, transform.position, transform.rotation);
+        HaveSpawned = true;
+    }
+}
--- Assets/Malthe/Scripts/SnapGrydeSkift.cs
+++ Assets/Malthe/Scripts/SnapGrydeSkift.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class SnapGrydeSkift : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject prefab1;
+    public GameObject prefab2;
+
+
+    public bool HaveSpawned = false;
+
     void Start()
     {
+        // Instantiate the first prefab
+        
+        // Invoke the SwitchObjects method after 3 seconds
+       
+    }
+
+    public void SwitchtoWater()
+    {
+        if (HaveSpawned == false)
+        {
+            Invoke("SwitchObjects", 3f);
+        }
+
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void SwitchObjects()
     {
-        
+        // Destroy the first prefab
+        Destroy(prefab1);
+
+        // Instantiate the second prefab
+        Instantiate(prefab2, transform.position, transform.rotation);
+        HaveSpawned = true;
     }
 }
